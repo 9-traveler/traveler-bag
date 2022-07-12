c719ac1545302548aff2ffd64733722dedb4edd4
9-traveler <108961846+9-traveler@users.noreply.github.com>
2022-07-12 11:36:16 +0800

    Add files via upload

--- test_7_11.c
+++ test_7_11.c
@@ -4,36 +4,119 @@
 
 
 
+
 int main()
 {
-	int day = 0;
-	scanf("%d",&day);
-	switch (day)
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
 	{
-	case 1:
-		printf("星期1\n");
-		break; 
-	case 2:
-		printf("星期2\n");
-		break;
-	case 3:
-		printf("星期3\n");
-		break;
-	case 4:
-		printf("星期4\n");
-		break;
-	case 5:
-		printf("星期5\n");
-		break;
-	case 6:
-		printf("星期6\n");
-		break;
-	case 7:
-		printf("星期7\n");
-		break;
+		putchar(ch);
 	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//	int i = 1;
+//	while(i<=199)
+//	{
+//		if (5 == i)
+//			continue;
+//	printf("%d ", i);
+//	i++;
+//    }
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch (n)
+//	{
+//	case 1:m++;
+//	case 2:n++;
+//	case 3:
+//			switch (n)
+//			{
+//			case 1:n++;
+//			case 2:m++; n++;
+//				break;
+//			}
+//	case 4:m++;
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("m=%d,n=%d\n", m, n);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d",&day);
+//	switch (day)
+//	{
+//	case 1: 
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("weekday\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("星期7\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
+//		break;
+//	
+//	
+//	}
+//	return 0;
+//}
 
 
 
